7f8deb88c1dea3d50ac666871531da28a7bd0fc2
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-12 21:33:59 -0400

    Agredando Radzen para las notificaciones

--- Tareas_1_Hasta_la_4/Client/Program.cs
+++ Tareas_1_Hasta_la_4/Client/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tareas_1_Hasta_la_4.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
