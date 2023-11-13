de08bacdb3ccf08287116cddaa6f742b358acb9f
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-12 20:58:54 -0400

    Creando la Clase Prioridades

---
+++ Tareas_1_Hasta_la_4/Shared/Prioridades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tareas_1_Hasta_la_4.Shared
+{
+    internal class Prioridades
+    {
+        [Key]
+        public int PrioridadesId { get; set; }
+        [Required(ErrorMessage ="!Debe llenar este campo¡")]
+        public string? Descripcion { get; set; }
+        [Range(1,int.MaxValue, ErrorMessage ="!Los dias de compromiso deben ser mayor que 1¡")]
+        public int DiasCompromiso { get; set; }
+    }
+}
