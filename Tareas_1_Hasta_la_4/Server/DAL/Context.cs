44953e1db4f6d529ba9b9defb976fd83a8bbbf06
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-12 21:05:36 -0400

    Agregamos Nuestro ConnectionString

--- Tareas_1_Hasta_la_4/Server/DAL/Context.cs
+++ Tareas_1_Hasta_la_4/Server/DAL/Context.cs
@@ -5,7 +5,7 @@ namespace Tareas_1_Hasta_la_4.Server.DAL
 {
     public class Context : DbContext
     {
-        DbSet<Prioridades> prioridades {  get; set; }
+        public DbSet<Prioridades> Prioridades {  get; set; }
         public Context(DbContextOptions<Context> options) : base(options){}
     }
 }
